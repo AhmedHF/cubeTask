c5f61454924d5fb82010423f0f7088a54d1a33c0
Ahmed Hassan <a.hassan19995@gmail.com>
2021-05-27 17:59:54 +0200

    add reacr qurey to app and provide the client to app

--- App.js
+++ App.js
@@ -6,20 +6,41 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Movies from './src/screens/Movies/Movies';
 import MovieDetails from './src/screens/MovieDetails/MovieDetails';
 
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const defaultQueryFn = async ({ queryKey }) => {
+  const { data } = await axios.get(`https://jsonplaceholder.typicode.com${queryKey[0]}`);
+  return data;
+};
+
+const queryClient = new QueryClient(
+  {
+    defaultOptions: {
+      queries: {
+        queryFn: defaultQueryFn,
+      },
+    },
+  }
+)
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Movies" component={Movies} />
-        <Stack.Screen
-          name="MovieDetails"
-          component={MovieDetails}
-          options={({ route }) => ({ title: route.params.name })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Movies" component={Movies} />
+          <Stack.Screen
+            name="MovieDetails"
+            component={MovieDetails}
+            options={({ route }) => ({ title: route.params.name })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      {/* <ReactQueryDevtools initialIsOpen /> */}
+    </QueryClientProvider>
   );
 }
 
